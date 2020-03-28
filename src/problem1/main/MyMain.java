7c37581ada1a4e044ee66bed4c64eece6c66b4d1
Hp <chetan.singh_da18@gla.ac.in>
2020-03-28 18:22:02 +0530

    final commit

--- src/problem1/main/MyMain.java
+++ src/problem1/main/MyMain.java
@@ -10,11 +10,11 @@ import problem1.mybst.MyBinarySearchTree;
 import java.util.Scanner;
 public class MyMain {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         MyBinarySearchTree bst = new MyBinarySearchTree();
         System.out.print("For Insertion press 1, for printing left nodes and nodes which who doesn't have left child press 2: ");
         int counter = sc.nextInt();
-        while(counter!=0) {
+        while (counter != 0) {
             switch (counter) {
                 case 1:
                     System.out.print("Enter No.: ");
