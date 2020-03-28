e4c35342050223c2705f76d0d1bb53477ac6cf0b
Hp <chetan.singh_da18@gla.ac.in>
2020-03-28 18:05:32 +0530

    initial commit

--- src/problem5/circularqueue/MyCircularQueue.java
+++ src/problem5/circularqueue/MyCircularQueue.java
@@ -5,6 +5,17 @@
  *  Time: 7:06 PM
  */
 package problem5.circularqueue;
-//to implement circular queue
+import problem5.node.Node;
+import problem5.student.Student;
+
 public class MyCircularQueue {
+    Node front;
+    Node rear;
+    int size;
+
+    public MyCircularQueue(){
+        front=null;
+        rear=null;
+        size=0;
+    }
 }
