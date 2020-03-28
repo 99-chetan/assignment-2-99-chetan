700bb215909f286a26a100b5d47050488373eda8
Hp <chetan.singh_da18@gla.ac.in>
2020-03-28 11:54:30 +0530

    initial commit

--- src/problem3/main/MyMain.java
+++ src/problem3/main/MyMain.java
@@ -10,5 +10,11 @@ import problem3.node.Node;
 import java.util.Scanner;
 
 public class MyMain {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        MyPriorityQueue obj=new MyPriorityQueue();
+        System.out.print("For Insertion press 1, for Dequeue press 2, for traversing press3, to print number of elements press 4: ");
+        int counter = sc.nextInt();
 
+    }
 }
