cefbe4b3bf175b056d0e8027c00d25fdcd48c0bc
Hp <chetan.singh_da18@gla.ac.in>
2020-03-28 17:39:48 +0530

    initial commit

--- src/problem4/myqueue/MyQueue.java
+++ src/problem4/myqueue/MyQueue.java
@@ -5,7 +5,15 @@
  *  Time: 7:11 PM
  */
 package problem4.myqueue;
-// to create queue to store pre - order successor
+import problem1.node.TreeNode;
+import problem4.node.Node;
 
 public class MyQueue {
+    Node front;
+    Node rear;
+
+    public MyQueue(){
+        front = null;
+        rear = null;
+    }
 }
