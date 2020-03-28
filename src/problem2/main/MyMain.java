6ca448ca7d5db4c84b9e0f81bacfdc5a7b7f2285
Hp <chetan.singh_da18@gla.ac.in>
2020-03-28 18:15:29 +0530

    initial commit

--- src/problem2/main/MyMain.java
+++ src/problem2/main/MyMain.java
@@ -5,7 +5,19 @@
  *  Time: 7:15 PM
  */
 package problem2.main;
-// executable class
-// use problem1.mybst.MyBinarySearchTree class for creating binary tree
+import problem1.mybst.MyBinarySearchTree;
+import problem1.node.TreeNode;
+
 public class MyMain {
+    public static void preorder(TreeNode root){
+        if(root == null){
+            return;
+        }
+        else{
+            System.out.print(root.getData()+"\t");
+            preorder(root.getLeft());
+            preorder(root.getRight());
+        }
+
+    }
 }
